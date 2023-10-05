a3f57e27f4fa6f4a1c4f76499a98b1019b1462d6
YumeiWang <yumei.wang@mohawkcollege.ca>
2023-10-05 15:59:27 -0400

    break convert_to_html down to sub functions

--- convert.py
+++ convert.py
@@ -28,8 +28,6 @@ def remake_til_folder():
     print(DEFAULT_OUTPUT + " folder created")
 
 def convert_to_html(path, output_folder, css_url):
-    """Function converting one .txt or .md TIL post to html"""
-
     # at slash at end of folder path if it is not there
     if output_folder[-1] != "/":
         output_folder += "/"
@@ -45,90 +43,145 @@ def convert_to_html(path, output_folder, css_url):
     # extract the file name and ext
     file_name = path_tup[0]
     file_ext = path_tup[1]
-    
-    # check if input file has a valid extension
-    if file_ext in (".txt", ".md"):
-        title = os.path.basename(file_name)
-        output_fname = title + ".html"
-
-        # use the input file name as the output file name
-        output_fpath = output_folder + output_fname
-
-        # flag which signifies if text is inside a code block
-        in_code_block = False
-
-        # open input and output files
-        with open(path, mode='r', encoding="utf-8") as input_file, open(output_fpath, mode='w', encoding="utf-8") as output_file:
-
-            # write html head
-            output_file.write('<!doctype html>\n')
-            output_file.write('<html lang="en">\n')
-            output_file.write('<head>\n')
-            output_file.write('\t<meta charset="utf-8">\n')
-            output_file.write(f'\t<title>{title}</title>\n')
-            output_file.write('\t<meta name="viewport" content="width=device-width, initial-scale=1">\n')
-            if css_url:
-                output_file.write(f'\t<link rel="stylesheet" href="{css_url}">\n')
-            output_file.write('</head>\n')
-
-            # write html body
-            output_file.write('<body>\n')
-
-            # read each line in input file
-            for line in input_file:
-
-                # trim the line of whitespace and newline character
-                line = line.strip()
-
-                if file_ext == ".md" :
-                    # If text is not inside a code block, must parse styled text
-                    if not in_code_block:
-                        # Look for beginning of code block (```)
-                        result_tup = re.subn(r'^[ ]*```.*$', r'<pre>', line)
-                        line = result_tup[0]
-                        num_found = result_tup[1]
-
-                        if num_found > 0:
-                            in_code_block = True
-                        else:
-                            # Replace *italic* and _italic_ with <em>italic</em>
-                            line = re.sub(r'([^*])\*([^*]+)\*', r'\1<em>\2</em>', line)
-                            line = re.sub(r'[^_]_([^_]+)_', r'<em>\1</em>', line)
-
-                            # Replace **bold** and __bold__ with <strong>bold</strong>
-                            line = re.sub(r'\*\*([^*]+)\*\*', r'<strong>\1</strong>', line)
-                            line = re.sub(r'__([^_]+)__', r'<strong>\1</strong>', line)
-
-                            # Replace --- with <hr />
-                            line = re.sub(r'^[ ]*---[-]*[ ]*$', r'<hr />', line)
-
-                            # Replace `text` with <code>text</code>
-                            line = re.sub(r'`(.*)`', r'<code>\1</code>', line)
-
-                            # Line is not in code block, so wrap line in paragraph tag
-                            line = "<p>" + line + "</p>"
-                    else:
-                        # Look for end of code block (```)
-                        result_tup = re.subn(r'^[ ]*```[`]*[ ]*$', r'</pre>', line)
-                        line = result_tup[0]
-                        num_found = result_tup[1]
-
-                        if num_found > 0:
-                            in_code_block = False
-                if line:
-                    # write the line to the output file
-                    output_file.write(f'{line}\n')
-                else:
-                    # write an empty line to the output file
-                    output_file.write(f'\n')
-
-            # write closing tags
-            output_file.write(f'</body>\n')
-            output_file.write(f'</html>')
-        
-        print(path.replace(os.sep, '/') + " converted to " + output_fpath + " successfully!")
+    if file_ext == ".md":
+        markdown_to_html(path,file_name, output_folder, css_url)
+    elif file_ext == ".md":
+        text_to_html(path,file_name, output_folder, css_url)
     else:
         print("Error: " + path.replace(os.sep, '/') + " was not converted. File extension should be .txt")
+
+
+
+def markdown_to_html(path,file_name, output_folder, css_url):
+    title = os.path.basename(file_name)
+    output_fname = title + ".html"
+
+    # use the input file name as the output file name
+    output_fpath = output_folder + output_fname
+
+    # flag which signifies if text is inside a code block
+    in_code_block = False
+
+    # open input and output files
+    with open(path, mode='r', encoding="utf-8") as input_file, open(output_fpath, mode='w', encoding="utf-8") as output_file:
+
+        # write html head
+        output_file.write('<!doctype html>\n')
+        output_file.write('<html lang="en">\n')
+        output_file.write('<head>\n')
+        output_file.write('\t<meta charset="utf-8">\n')
+        output_file.write(f'\t<title>{title}</title>\n')
+        output_file.write('\t<meta name="viewport" content="width=device-width, initial-scale=1">\n')
+        if css_url:
+            output_file.write(f'\t<link rel="stylesheet" href="{css_url}">\n')
+        output_file.write('</head>\n')
+
+        # write html body
+        output_file.write('<body>\n')
+
+        # read each line in input file
+        for line in input_file:
+
+            # trim the line of whitespace and newline character
+            line = line.strip()
+
+            
+                # If text is not inside a code block, must parse styled text
+            if not in_code_block:
+                # Look for beginning of code block (```)
+                result_tup = re.subn(r'^[ ]*```.*$', r'<pre>', line)
+                line = result_tup[0]
+                num_found = result_tup[1]
+
+                if num_found > 0:
+                    in_code_block = True
+                else:
+                    # Replace *italic* and _italic_ with <em>italic</em>
+                    line = re.sub(r'([^*])\*([^*]+)\*', r'\1<em>\2</em>', line)
+                    line = re.sub(r'[^_]_([^_]+)_', r'<em>\1</em>', line)
+
+                    # Replace **bold** and __bold__ with <strong>bold</strong>
+                    line = re.sub(r'\*\*([^*]+)\*\*', r'<strong>\1</strong>', line)
+                    line = re.sub(r'__([^_]+)__', r'<strong>\1</strong>', line)
+
+                    # Replace --- with <hr />
+                    line = re.sub(r'^[ ]*---[-]*[ ]*$', r'<hr />', line)
+
+                    # Replace `text` with <code>text</code>
+                    line = re.sub(r'`(.*)`', r'<code>\1</code>', line)
+
+                    # Line is not in code block, so wrap line in paragraph tag
+                    line = "<p>" + line + "</p>"
+            else:
+                # Look for end of code block (```)
+                result_tup = re.subn(r'^[ ]*```[`]*[ ]*$', r'</pre>', line)
+                line = result_tup[0]
+                num_found = result_tup[1]
+
+                if num_found > 0:
+                    in_code_block = False
+            if line:
+                # write the line to the output file
+                output_file.write(f'{line}\n')
+            else:
+                # write an empty line to the output file
+                output_file.write(f'\n')
+
+        # write closing tags
+        output_file.write(f'</body>\n')
+        output_file.write(f'</html>')
+    
+    print(path.replace(os.sep, '/') + " converted to " + output_fpath + " successfully!")
+
+
+def text_to_html(path,file_name, output_folder, css_url):
+    # check if input file has a valid extension
+    title = os.path.basename(file_name)
+    output_fname = title + ".html"
+
+    # use the input file name as the output file name
+    output_fpath = output_folder + output_fname
+
+    # flag which signifies if text is inside a code block
+    in_code_block = False
+
+    # open input and output files
+    with open(path, mode='r', encoding="utf-8") as input_file, open(output_fpath, mode='w', encoding="utf-8") as output_file:
+
+        # write html head
+        output_file.write('<!doctype html>\n')
+        output_file.write('<html lang="en">\n')
+        output_file.write('<head>\n')
+        output_file.write('\t<meta charset="utf-8">\n')
+        output_file.write(f'\t<title>{title}</title>\n')
+        output_file.write('\t<meta name="viewport" content="width=device-width, initial-scale=1">\n')
+        if css_url:
+            output_file.write(f'\t<link rel="stylesheet" href="{css_url}">\n')
+        output_file.write('</head>\n')
+
+        # write html body
+        output_file.write('<body>\n')
+
+        # read each line in input file
+        for line in input_file:
+
+            # trim the line of whitespace and newline character
+            line = line.strip()
+
+
+            if line:
+                # write the line to the output file
+                output_file.write(f'{line}\n')
+            else:
+                # write an empty line to the output file
+                output_file.write(f'\n')
+
+        # write closing tags
+        output_file.write(f'</body>\n')
+        output_file.write(f'</html>')
+    
+    print(path.replace(os.sep, '/') + " converted to " + output_fpath + " successfully!")
+
 
 # only triggered when we call this .py file and not during imports
 if __name__ == '__main__':
